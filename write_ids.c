cbc3d93281e478e3ab56d8b9bd062c9a181f7823
Evan Edris <evanedris@yahoo.com>
2024-09-10 21:19:46 -0400

    Id changed.

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "EvanEdris";
+    char STUDENT_ID[] = "962627229";
 
     return 0 ;
 }
